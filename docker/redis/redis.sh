1f0db1e9c0bce8e44ba376d1a4c3b417e03a699f
yTsunomoto <wasiwasimaru@gmail.com>
2019-08-17 12:26:16 +0900

    update 設定を更新。

--- docker/redis/redis.sh
+++ docker/redis/redis.sh
@@ -1,68 +1,68 @@
 #!/bin/bash
 
 rm -f \
-    /data/conf/r7000i.log \
-    /data/conf/r7001i.log \
-    /data/conf/r7002i.log \
-    /data/conf/r7003i.log \
-    /data/conf/r7004i.log \
-    /data/conf/r7005i.log \
-    /data/conf/nodes.7000.conf \
-    /data/conf/nodes.7001.conf \
-    /data/conf/nodes.7002.conf \
-    /data/conf/nodes.7003.conf \
-    /data/conf/nodes.7004.conf \
-    /data/conf/nodes.7005.conf ;
+    /data/conf/r7100i.log \
+    /data/conf/r7101i.log \
+    /data/conf/r7102i.log \
+    /data/conf/r7103i.log \
+    /data/conf/r7104i.log \
+    /data/conf/r7105i.log \
+    /data/conf/nodes.7100.conf \
+    /data/conf/nodes.7101.conf \
+    /data/conf/nodes.7102.conf \
+    /data/conf/nodes.7103.conf \
+    /data/conf/nodes.7104.conf \
+    /data/conf/nodes.7105.conf ;
 
-redis-server /data/conf/redis.conf --port 7000 --cluster-config-file /data/conf/nodes.7000.conf --daemonize yes ;
-redis-server /data/conf/redis.conf --port 7001 --cluster-config-file /data/conf/nodes.7001.conf --daemonize yes ;
-redis-server /data/conf/redis.conf --port 7002 --cluster-config-file /data/conf/nodes.7002.conf --daemonize yes ;
-redis-server /data/conf/redis.conf --port 7003 --cluster-config-file /data/conf/nodes.7003.conf --daemonize yes ;
-redis-server /data/conf/redis.conf --port 7004 --cluster-config-file /data/conf/nodes.7004.conf --daemonize yes ;
-redis-server /data/conf/redis.conf --port 7005 --cluster-config-file /data/conf/nodes.7005.conf --daemonize yes ;
+redis-server /data/conf/redis.conf --port 7100 --cluster-config-file /data/conf/nodes.7100.conf --daemonize yes ;
+redis-server /data/conf/redis.conf --port 7101 --cluster-config-file /data/conf/nodes.7101.conf --daemonize yes ;
+redis-server /data/conf/redis.conf --port 7102 --cluster-config-file /data/conf/nodes.7102.conf --daemonize yes ;
+redis-server /data/conf/redis.conf --port 7103 --cluster-config-file /data/conf/nodes.7103.conf --daemonize yes ;
+redis-server /data/conf/redis.conf --port 7104 --cluster-config-file /data/conf/nodes.7104.conf --daemonize yes ;
+redis-server /data/conf/redis.conf --port 7105 --cluster-config-file /data/conf/nodes.7105.conf --daemonize yes ;
 
 REDIS_LOAD_FLG=true;
 
 while $REDIS_LOAD_FLG;
 do
     sleep 1;
-    redis-cli -p 7000 info 1> /data/conf/r7000i.log 2> /dev/null;
-    if [ -s /data/conf/r7000i.log ]; then
+    redis-cli -p 7100 info 1> /data/conf/r7100i.log 2> /dev/null;
+    if [ -s /data/conf/r7100i.log ]; then
         :
     else
         continue;
     fi
-    redis-cli -p 7001 info 1> /data/conf/r7001i.log 2> /dev/null;
-    if [ -s /data/conf/r7001i.log ]; then
+    redis-cli -p 7101 info 1> /data/conf/r7101i.log 2> /dev/null;
+    if [ -s /data/conf/r7101i.log ]; then
         :
     else
         continue;
     fi
-    redis-cli -p 7002 info 1> /data/conf/r7002i.log 2> /dev/null;
-    if [ -s /data/conf/r7002i.log ]; then
+    redis-cli -p 7102 info 1> /data/conf/r7102i.log 2> /dev/null;
+    if [ -s /data/conf/r7102i.log ]; then
         :
     else
         continue;
     fi
-    redis-cli -p 7003 info 1> /data/conf/r7003i.log 2> /dev/null;
-    if [ -s /data/conf/r7003i.log ]; then
+    redis-cli -p 7103 info 1> /data/conf/r7103i.log 2> /dev/null;
+    if [ -s /data/conf/r7103i.log ]; then
         :
     else
         continue;
     fi
-    redis-cli -p 7004 info 1> /data/conf/r7004i.log 2> /dev/null;
-    if [ -s /data/conf/r7004i.log ]; then
+    redis-cli -p 7104 info 1> /data/conf/r7104i.log 2> /dev/null;
+    if [ -s /data/conf/r7104i.log ]; then
         :
     else
         continue;
     fi
-    redis-cli -p 7005 info 1> /data/conf/r7005i.log 2> /dev/null;
-    if [ -s /data/conf/r7005i.log ]; then
+    redis-cli -p 7105 info 1> /data/conf/r7105i.log 2> /dev/null;
+    if [ -s /data/conf/r7105i.log ]; then
         :
     else
         continue;
     fi
-    yes "yes" | redis-cli --cluster create 127.0.0.1:7000 127.0.0.1:7001 127.0.0.1:7002 127.0.0.1:7003 127.0.0.1:7004 127.0.0.1:7005 --cluster-replicas 1;
+    yes "yes" | redis-cli --cluster create 127.0.0.1:7100 127.0.0.1:7101 127.0.0.1:7102 127.0.0.1:7103 127.0.0.1:7104 127.0.0.1:7105 --cluster-replicas 1;
     REDIS_LOAD_FLG=false;
 done
 
